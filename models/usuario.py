0717ae4a377d1509ae648704eefb6019ffe0ec14
marceloperini <marceloprni@gmail.com>
2022-01-04 22:22:18 -0300

    UPDATE PROGRAM
    DEVELOP PART OF MAIN, MODELS, RESOURCES

--- models/usuario.py
+++ models/usuario.py
@@ -1,21 +1,49 @@
 from sql_alchemy import banco
+from flask import request, url_for
+from requests import post
+
+MAILGUN_API_KEY = '57fd86c964f3bc345b1688afa7d572a7-cac494aa-2ccd487c'
+FROM_TITLE = 'No-Riple'
+FROM_EMAIL = 'no_reply@restapi.com'
+MAILGUN_DOMAIN = 'sandbox0a063f878b7340a29adfd88efbba530b.mailgun.org'
 
 class UserModel(banco.Model):
     __tablename__ = 'usuarios'
 
     user_id = banco.Column(banco.Integer, primary_key=True)
-    login = banco.Column(banco.String(80))
-    senha = banco.Column(banco.String(40))
+    login = banco.Column(banco.String(80), nullable=False, unique=True)
+    senha = banco.Column(banco.String(40), nullable=False)
+    email = banco.Column(banco.String(80), nullable=False, unique=True)
+    ativado = banco.Column(banco.Boolean, default=False)
 
-    def __init__(self, login, senha):
+    def __init__(self, login, senha, email,ativado):
         self.login = login
         self.senha = senha
+        self.email = email
+        self.ativado = ativado
+    
+    def send_confirmation_email(self):
+        # http://127.0.0.1:5000/confirmacao/{user_id}
+        link = request.url_root[:-1] + url_for('userconfirm', user_id=self.user_id)
+        return post('https://api.mailgun.net/v3/{}/messages'.format(MAILGUN_DOMAIN), 
+               auth=('api', MAILGUN_API_KEY), 
+               data={'from': '{} <{}>'.format(FROM_TITLE, FROM_EMAIL),
+                     'to': self.email,
+                     'subject' : 'Confirmação de Cadastro',
+                     'text': 'Confirme seu cadastro clicando no link a seguir: {}'.format(link),
+                     'html': '<html><p>\
+                      Confirme seu cadastro clicando no link a seguir: <a href="{}">CONFIRMAR EMAIL</a>\
+                      </p></html>'.format(link)
+                    }
+                )
+                     
         
-
     def json(self):
         return {
             'user_id': self.user_id,
-            'login': self.login 
+            'login': self.login, 
+            'email': self.email,
+            'ativado': self.ativado
         }  
 
     @classmethod
@@ -36,6 +64,13 @@ class UserModel(banco.Model):
     @classmethod
     def find_by_login(cls, login):
         user = cls.query.filter_by(login=login).first()
+        if user:
+            return user
+        return None
+
+    @classmethod
+    def find_by_email(cls, email):
+        user = cls.query.filter_by(email=email).first()
         if user:
             return user
         return None
--- resources/usuario.py
+++ resources/usuario.py
@@ -3,11 +3,13 @@ from models.usuario import UserModel
 from flask_jwt_extended import create_access_token, jwt_required, get_jwt
 from werkzeug.security import safe_str_cmp
 from blacklist import BLACKLIST 
+import traceback
 
 atributos = reqparse.RequestParser()
 atributos.add_argument('login', type=str, required=True, help="The field 'login' cannot be left blank.")
 atributos.add_argument('senha', type=str, required=True, help="The field 'senha' cannot be left blank.")
-
+atributos.add_argument('email', type=str)
+atributos.add_argument('ativado', type=bool)
 class User(Resource):
     # /usuarios/{user_id}
     def get(self, user_id):
@@ -28,12 +30,24 @@ class UserRegister(Resource):
     # /cadastro
     def post(self):
         dados = atributos.parse_args()
+        if not dados.get('email') or dados.get('email') is None:
+            return {"message": "The field 'email' cannot be left blank." }, 400
+        
+        if UserModel.find_by_email(dados['email']):
+            return {"message": "The email '{}' already exists.".format(dados['email'])}, 400
 
         if UserModel.find_by_login(dados['login']):
             return {"message": "The login '{}' already exists.".format(dados['login'])}
 
         user = UserModel(**dados)
-        user.save_user()
+        user.ativado = False
+        try:
+            user.save_user()
+            user.send_confirmation_email()
+        except:
+            user.delete_user()
+            traceback.print_exc()
+            return {"message": 'An internal server error has ocurred.'}, 500
         return {'message': 'User created successfully!'}, 201 # Created
 
 class UserLogin(Resource):
@@ -45,8 +59,11 @@ class UserLogin(Resource):
         user = UserModel.find_by_login(dados['login'])
 
         if user and safe_str_cmp(user.senha, dados['senha']):
-            token_de_acesso = create_access_token(identity=user.user_id)
-            return {'access_token': token_de_acesso}, 200
+            if user.ativado:
+
+                token_de_acesso = create_access_token(identity=user.user_id)
+                return {'access_token': token_de_acesso}, 200
+            return{'message': 'User not confirmed.'}, 400
         return {'message': 'The username or password is incorrect.'}, 401 # Unauthorized
 
 
@@ -57,3 +74,16 @@ class UserLogout(Resource):
         jwt_id = get_jwt()['jti'] # JWT Token Identifier
         BLACKLIST.add(jwt_id)
         return {'message': 'Logged out successfully!', 'id': jwt_id}, 200
+
+class UserConfirm(Resource):
+    
+    @classmethod
+    def get(cls, user_id):
+        user = UserModel.find_user(user_id)
+
+        if not user:
+            return {"message": "User id '{}' not found.".format(user_id)}, 404
+    
+        user.ativado = True
+        user.save_user()
+        return {"message": "User id '{}' confirmed sucesfully.".format(user_id)}
